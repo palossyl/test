c0926a3f58679a767f79ec0a0abf214906504acd
Laszlo Palossy <palossy.laszlo@gmail.com>
2019-09-23 12:31:47 +0200

    Hello hangyász

--- src/main/java/com/laci/test/App.java
+++ src/main/java/com/laci/test/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello tengerész!" );
-        System.out.println( "Hello Sanyi!" );
+        System.out.println( "Hello hangyász!" );
         System.out.println( "Hello Pist!" );
         System.out.println( "Hello Oszi!" );
         System.out.println( "Hello Kató!" );
