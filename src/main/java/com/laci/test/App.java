a8875616b3598e3e512bbccafcf139a2eadade01
Laszlo Palossy <palossy.laszlo@gmail.com>
2019-09-23 13:27:51 +0200

    cápi konflikt

--- src/main/java/com/laci/test/App.java
+++ src/main/java/com/laci/test/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello tengerész!" );
-        System.out.println( "Hello hangyász!" ); // feature cápi
+        System.out.println( "Hello hangyász!" );
         System.out.println( "Hello Pist!" );
         System.out.println( "Hello Oszi!" );
         System.out.println( "Hello Kató!" );
@@ -18,4 +18,6 @@ public class App
         //ponty
         //keszeg
     }
+    
+    //kkkkk
 }
